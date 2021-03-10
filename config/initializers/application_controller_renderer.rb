1ab21590b6a18832a6e7dcca05003b21a2704441
simplly <takuyax0806@docomo.ne.jp>
2021-03-10 12:29:52 +0000

    start

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1cb15a338d2337451c9a4780bf6214ec7784c0b31b19ef0656b86c8f6e342b36f7b2bf8f7122604441173456946ccee22ef50cbd1d2e179c3a62a02c8997b214'
